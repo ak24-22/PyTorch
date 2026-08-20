b0e847d1b837f0a1cd0f07b0e41cf19cb59ba927
AK <aliabdulkareem942@gmail.com>
2026-08-20 18:29:55 +0100

    Renamed the file and added notes

---
+++ Tutorial/PyTorch-Notes.py
@@ -0,0 +1,9 @@
+# YouTube Course Title:PyTorch for Deep Learning & Machine Learning – Full Course: By freeCodeCamp.org
+
+# My notes
+
+# Machine Learning: A system that improves its performance on a task with more data or experience, enabling it to make predictions and decisions. It does this by turning any data types (audio files, images, text, videos, tables of numbers, etc...) into numbers and finding patterns in those numbers.
+
+# Deep Learning: A system that goes through and analyses multiple layers of computation using neural networks to learn the patterns. This is a topic that is studied in Computer Science and is a subfield of Machine Learning which is a subfield of Artificial Intelligence.
+
+
