31452123a5030b4c57d6a598d97e9aba09570d1a
AK <aliabdulkareem942@gmail.com>
2026-08-21 21:12:05 +0100

    Added ML/DL examples

--- Tutorial/PyTorch-Notes.py
+++ Tutorial/PyTorch-Notes.py
@@ -2,8 +2,13 @@
 
 # My notes
 
+# Machine Learning (ML) and Deep Learning (DL) are very similar and used interchangebly in this YT Course.
+
 # Machine Learning: A system that improves its performance on a task with more data or experience, enabling it to make predictions and decisions. It does this by turning any data types (audio files, images, text, videos, tables of numbers, etc...) into numbers and finding patterns in those numbers.
 
 # Deep Learning: A system that goes through and analyses multiple layers of computation using neural networks to learn the patterns. This is a topic that is studied in Computer Science and is a subfield of Machine Learning which is a subfield of Artificial Intelligence.
 
-
+# When to use ML/DL and what's it good for 
+# --> Long or complex tasks, e.g., Driving a car properly
+# --> Needs Adapting, e.g., A smart thermostat that can change depending on the season
+# -->  Massive datasets, e.g., Looks at 100 different dishes and identifies them
