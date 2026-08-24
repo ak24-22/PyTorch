3070fada7ebfa916a61c27592bfe8f08e5dd02d5
AK <aliabdulkareem942@gmail.com>
2026-08-24 19:38:21 +0100

    Added ML v DL

--- Tutorial/PyTorch-Notes.py
+++ Tutorial/PyTorch-Notes.py
@@ -19,3 +19,12 @@
 # --> Flawless and 0 errors, e.g., Calculating chemotherapy dosages 
 # --> Not enough data, e.g., predicting a rare disease
 
+# ML vs DL (Traditionally, and in general)
+
+# ML --> Structured data with tables, columns and rows, an example of a good algorithm for this is eXtreme Gradient Boost (XGBoost). this is used in Data Science cometitions and on the internet and Apps, another one is a Tree based algorithm, Other algorithms are Random Forest, Gradient Boosted Models, Naive Bayes, Nearest Neighbour, Support Vector Machine, and many more... These are known as Shallow Algorithms due to the rise of deep learning  
+
+# DL --> Utructured data with different data format, a good algorithm for this is Neural Networks, so this would text/language formats and image formats and include audio formats Common Algorithms for deep learning are Fully Connected neural networks, Convolutional neural networks, Recurrent neural networks, Transformer and meny more...
+
+# Course Focuses on: Neural Networks, Fully Connected neural networks and Convolutional neural networks
+
+# Depending on users circumstances or situation, they could use ML for structured data or unstructured data and use DL for structured data or unstructured data
