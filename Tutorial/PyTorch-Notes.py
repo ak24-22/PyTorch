ee86f1ac01bc8b3dbdbac02d4d568f87c24b89c5
AK <aliabdulkareem942@gmail.com>
2026-08-25 14:27:07 +0100

    Added Neural Network topic

--- Tutorial/PyTorch-Notes.py
+++ Tutorial/PyTorch-Notes.py
@@ -2,29 +2,40 @@
 
 # My notes
 
-# Machine Learning (ML) and Deep Learning (DL) are very similar and used interchangebly in this YT Course.
-
+# 1. WHAT ARE MACHINE LEARNING (ML) AND DEEP LEARNING (DL)?
+# Machine Learning (ML) and Deep Learning (DL) are very similar. (Used interchangebly in this YT Course.)
 # Machine Learning: A system that improves its performance on a task with more data or experience, enabling it to make predictions and decisions. It does this by turning any data types (audio files, images, text, videos, tables of numbers, etc...) into numbers and finding patterns in those numbers.
-
 # Deep Learning: A system that goes through and analyses multiple layers of computation using neural networks to learn the patterns. This is a topic that is studied in Computer Science and is a subfield of Machine Learning which is a subfield of Artificial Intelligence.
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# 2. WHEN TO USE ML/DL AND WHAT'S IT GOOD FOR?
 # When to use ML/DL and what's it good for 
 # --> Long or complex tasks, e.g., Driving a car properly
 # --> Needs adapting, e.g., A smart thermostat that can change depending on the season
 # -->  Massive datasets, e.g., Looks at 100 different dishes and identifies them
 
-# When not to use ML/DL and what's it's not good for 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# 3. WHEN NOT TO USE ML/DL AND WHAT'S IT'S NOT GOOD FOR?
 # --> Short or simple tasks, e.g., Turning lights on or off
 # --> Needs explaining, e.g., Medical diagnosis  
 # --> Flawless and 0 errors, e.g., Calculating chemotherapy dosages 
 # --> Not enough data, e.g., predicting a rare disease
 
-# ML vs DL (Traditionally, and in general)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# ML --> Structured data with tables, columns and rows, an example of a good algorithm for this is eXtreme Gradient Boost (XGBoost). this is used in Data Science cometitions and on the internet and Apps, another one is a Tree based algorithm, Other algorithms are Random Forest, Gradient Boosted Models, Naive Bayes, Nearest Neighbour, Support Vector Machine, and many more... These are known as Shallow Algorithms due to the rise of deep learning  
-
-# DL --> Utructured data with different data format, a good algorithm for this is Neural Networks, so this would text/language formats and image formats and include audio formats Common Algorithms for deep learning are Fully Connected neural networks, Convolutional neural networks, Recurrent neural networks, Transformer and meny more...
-
+# 4. MACHINE LEARNING (ML) VS DEEP LEARNING (DL) (Traditionally, and in general)
+# ML --> Structured data with tables, columns and rows, an example of a good algorithm for this is eXtreme Gradient Boost (XGBoost). this is used in Data Science competitions and on the internet and Apps, another one is a Tree based algorithm, Other algorithms are Random Forest, Gradient Boosted Models, Naive Bayes, Nearest Neighbour, Support Vector Machine, and many more... These are known as Shallow Algorithms due to the rise of deep learning  
+# DL --> Unstructured data with different data format, a good algorithm for this is Neural Networks, so this would text/language formats and image formats and include audio formats Common Algorithms for deep learning are Fully Connected neural networks, Convolutional neural networks, Recurrent neural networks, Transformer and many more...
 # Course Focuses on: Neural Networks, Fully Connected neural networks and Convolutional neural networks
-
 # Depending on users circumstances or situation, they could use ML for structured data or unstructured data and use DL for structured data or unstructured data
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# 5. WHAT ARE NEURAL NETWORKS?
+# The term “neural network” is a mathematical model that computes and analyses data. This is used in computer science and the full term is called a deep neural network because the model has multiple layers, meaning it goes through multiple processing steps before producing an answer
+
+# In general these are the steps of Neural Network, Inputs the raw data this can be text, images, audio, etc... Then it's the preprocessing/Numerical encoding step which turn the raw data into numbers. THen The neural network will first input the raw data the second layer is the Hidden layer and that's were it learns the representation (Also known as patterns or features or weights) by going through each node (i.e. Network of Neurons) and outputs the data. Then comes the postprocessingRepresentation output step which are decimal numbers (also called probabilities or vectors) and the last step outputs which results in human-readable understanding 
+
+
